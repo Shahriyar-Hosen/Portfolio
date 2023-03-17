c266f77fb0a50cfee5521d0cb9deb13b864112ae
shahriyarhosen <me.shahriyarhosen@gmail.com>
2023-03-17 13:07:03 +0600

    logo add and tailwind css error solved

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { memo, useState } from "react";
+import { Link } from "react-router-dom";
+import { styles } from "@/styles";
 
-const Navbar = () => {
+import logo from "~/assets/logo.svg";
+
+const Navbar = memo(() => {
+  const [active, setActive] = useState("");
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex
+       items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+        </Link>
+      </div>
+    </nav>
+  );
+});
 
-export default Navbar
+export default Navbar;
