82d7404c93972b3a97dce179ddba5bd39bd177f7
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2023-03-28 16:40:30 +0600

    Contact section full done

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -37,9 +37,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Shahriyar Hosen",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: import.meta.env.VITE_APP_TO_EMAIL,
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
